70c30a356b29c8d2a859bd0200ea5c48b2259348
rd <liuyongcheng01@cnpc.com.cn>
2022-09-09 09:16:11 +0800

    debug

--- src/main/java/hello/controller/TaskController.java
+++ src/main/java/hello/controller/TaskController.java
@@ -1,11 +1,19 @@
 package hello.controller;
 
 import com.test.data.TestMe;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
 /**
  * Created by richard on 14/03/2019.
@@ -15,11 +23,18 @@ import javax.servlet.http.HttpServletResponse;
 @RequestMapping("/data")
 public class TaskController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(TaskController.class);
+
     @RequestMapping("/test")
     @ResponseBody
-    public long test() {
+    public String test() {
 
-        return System.currentTimeMillis();
+        String cmd[] = new String[2];
+        cmd[0] = "/usr/bin/sh";
+        cmd[1] = "get_time.sh";
+        return this.execShell(cmd).getBody();
+
+//        return System.currentTimeMillis();
 
 //        return "{\n" +
 //                "  \"dataset\":\n" +
@@ -53,5 +68,55 @@ public class TaskController {
 //        response.addHeader("Content-Type", "text/plain;charset=UTF-8");
 
         return "{\"a\":\"b\"}";
+    }
+
+    public ResponseEntity<String> execShell(final String[] cmds) {
+        LOGGER.info("cmd = {}", Arrays.toString(cmds).replace(",", "").replace(" ", "' '"));
+        ResponseEntity<String> responseEntity = null;
+        Process process = null;
+        BufferedReader stdInput = null;
+        BufferedReader stdError = null;
+        try {
+            process = Runtime.getRuntime().exec(cmds);
+            stdInput = new BufferedReader(
+                new InputStreamReader(process.getInputStream())
+            );
+            stdError = new BufferedReader(
+                new InputStreamReader(process.getErrorStream())
+            );
+
+            String tmp;
+            final StringBuilder stdout = new StringBuilder(32);
+            final StringBuilder stderr = new StringBuilder(32);
+            while ((tmp = stdInput.readLine()) != null) {
+                stdout.append(tmp);
+                stdout.append("\r\n");
+
+            }
+            responseEntity = new ResponseEntity<>(stdout.toString(), HttpStatus.OK);
+            LOGGER.info("stdout of cmd:{}", stdout.toString());
+            while ((tmp = stdError.readLine()) != null) {
+                stderr.append(tmp);
+                stderr.append("\r\n");
+            }
+            LOGGER.info("stderr of cmd:\n{}", stderr.toString());
+
+        } catch (IOException e) {
+            LOGGER.error("error", e);
+            responseEntity = new ResponseEntity<>(e.toString(), HttpStatus.BAD_REQUEST);
+        } finally {
+            try {
+                stdInput.close();
+            } catch (IOException e) {
+                LOGGER.error("error", e);
+            }
+            try {
+                stdError.close();
+            } catch (IOException e) {
+                LOGGER.error("error", e);
+            }
+            process.destroyForcibly();
+        }
+        return responseEntity;
     }
 }
